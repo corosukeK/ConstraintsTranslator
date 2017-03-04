a9307ad8e69671e4f6c1f2142d8ed38883907797
akuraru <akuraru@gmail.com>
2017-03-04 12:06:37 +0900

    テスト対象のオブジェクトを作る

--- ConstrainsTranslatorTests/ConstrainsTranslatorTests.swift
+++ ConstrainsTranslatorTests/ConstrainsTranslatorTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import ConstraintsTranslator
 
 class ConstrainsTranslatorTests: XCTestCase {
     
@@ -18,10 +19,34 @@ class ConstrainsTranslatorTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testTemporaryClassToJsonFormat() {
+        let view = View(id: "m2d-tO-WM7", subviews: [
+            View(id: "Nb9-oQ-WCJ", subviews: [], constrains: [
+                Constraint(
+                    firstAttribute: "width",
+                    constant: "100",
+                    id: "COE-ui-HIh"
+                ), Constraint(
+                    firstAttribute: "height",
+                    constant: "100",
+                    id: "Uvy-fQ-vpj"
+                )
+                
+                ])
+            ] , constrains:[
+                Constraint(
+                    firstItem: "Nb9-oQ-WCJ",
+                    firstAttribute: "centerY",
+                    secondItem: "m2d-tO-WM7",
+                    secondAttribute: "centerY",
+                    id: "lsJ-1m-vgh"
+                ), Constraint(
+                    firstItem: "Nb9-oQ-WCJ",
+                    firstAttribute: "centerX",
+                    secondItem: "m2d-tO-WM7",
+                    secondAttribute: "centerX",
+                    id: "nTa-fY-4GG"
+                )
+            ])
     }
-    
-    func testPerformanceExample() {
-    }
-    
 }
--- Sources/VisualFormatLanguageTranslator.swift
+++ Sources/VisualFormatLanguageTranslator.swift
@@ -15,11 +15,28 @@ class VisualFormatLanguageTranslator {
 }
 
 struct Constraint {
-    let firstItem: String
+    let firstItem: String?
     let firstAttribute: String
-    let secondItem: String
-    let secondAttribute: String
+    let secondItem: String?
+    let secondAttribute: String?
+    let constant: String?
     let id:String
+    
+    init(
+        firstItem: String? = nil,
+        firstAttribute: String,
+        secondItem: String? = nil,
+        secondAttribute: String? = nil,
+        constant: String? = nil,
+        id:String
+        ) {
+        self.firstItem = firstItem
+        self.firstAttribute = firstAttribute
+        self.secondItem = secondItem
+        self.secondAttribute = secondAttribute
+        self.constant = constant
+        self.id = id
+    }
 }
 
 struct View {
