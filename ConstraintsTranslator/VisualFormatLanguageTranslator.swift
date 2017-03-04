85fd0eac6c8acd84ee25b09868cab5f6ddca1f34
KeisukeKimura <corosuke0227@me.com>
2017-03-04 10:14:53 +0900

    Add VisualFormatLanguageTranslator file

---
+++ ConstraintsTranslator/VisualFormatLanguageTranslator.swift
@@ -0,0 +1,15 @@
+//
+//  VisualFormatLanguageTranslator.swift
+//  ConstraintsTranslator
+//
+//  Created by KeisukeKimura on 2017/03/04.
+//  Copyright © 2017年 Keisuke Kimura. All rights reserved.
+//
+
+import Foundation
+
+class VisualFormatLanguageTranslator {
+    static func translate(storyboardString: String) -> String {
+        return ""
+    }
+}
